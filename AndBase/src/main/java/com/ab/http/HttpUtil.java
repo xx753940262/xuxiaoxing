66621302a126a2e1d9f400777e9e32e0f6bf2606
xuxing <xu@qq.com>
2016-08-12 17:13:49 +0800

    andbase添加get请求

--- AndBase/src/main/java/com/ab/http/HttpUtil.java
+++ AndBase/src/main/java/com/ab/http/HttpUtil.java
@@ -30,15 +30,23 @@ public class HttpUtil {
 
 
     public static void httpClientPostSend(final Context response, AbRequestParams params, final String url) {
-        httpClientSend((OnMessageResponse) response, params, url);
+        httpPostClientSend((OnMessageResponse) response, params, url);
     }
 
     public static void httpClientFragmentPostSend(final Fragment response, AbRequestParams params, final String url) {
-        httpClientSend((OnMessageResponse) response, params, url);
+        httpPostClientSend((OnMessageResponse) response, params, url);
     }
 
-    private static void httpClientSend(final OnMessageResponse response, AbRequestParams params, final String url) {
-        AbLogUtil.e("SERVICE_URL==", SERVICE_URL + url);
+    public static void httpClientGetSend(final Context response, AbRequestParams params, final String url) {
+        httpGetClientSend((OnMessageResponse) response, params, url);
+    }
+
+    public static void httpClientFragmentGetSend(final Fragment response, AbRequestParams params, final String url) {
+        httpGetClientSend((OnMessageResponse) response, params, url);
+    }
+
+    private static void httpPostClientSend(final OnMessageResponse response, AbRequestParams params, final String url) {
+        AbLogUtil.e("SERVICE_Post_URL==", SERVICE_URL + url);
 
         if (response instanceof Fragment) {
             context = ((Fragment) response).getActivity();
@@ -51,8 +59,76 @@ public class HttpUtil {
                     @Override
                     public void onSuccess(int statusCode, String content) {
 
-                        content = AbBase64.decode(content,"UTF-8");
-                        Log.e("接口返回的数据=",content);
+                        content = AbBase64.decode(content, "UTF-8");
+                        Log.e("接口返回的数据=", content);
+                        try {
+                            JSONObject jo = new JSONObject(content);
+                            response.onMessageResponse(url, jo);
+                        } catch (JSONException e) {
+                            JSONArray jsonArray = null;
+                            try {
+                                jsonArray = new JSONArray(content);
+                                response.onMessageResponse(url, jsonArray);
+
+                            } catch (JSONException e1) {
+                                e1.printStackTrace();
+                                try {
+                                    response.onMessageResponse(url, jsonArray);
+                                } catch (JSONException e2) {
+                                    e2.printStackTrace();
+                                }
+                            }
+
+                        }
+                    }
+
+                    @Override
+                    public void onStart() {
+                    }
+
+                    @Override
+                    public void onFinish() {
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, String content,
+                                          Throwable error) {
+                        try {
+                            AbLogUtil.e("statusCode==", statusCode + "");
+                            AbLogUtil.e("content==", content + "");
+                            AbLogUtil.e("error==", error + "");
+                            if (statusCode == 600) {
+                                AbToastUtil.showToast(context, content);
+                            } else if (statusCode == 900) {
+                                AbToastUtil.showToast(context, "连接服务器失败，请检查服务器是否正常.");
+                            }
+                            response.onMessageResponse(url, "");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+
+        );
+    }
+
+    private static void httpGetClientSend(final OnMessageResponse response, AbRequestParams params, final String url) {
+        AbLogUtil.e("SERVICE_GET_URL==", SERVICE_URL + url);
+
+        if (response instanceof Fragment) {
+            context = ((Fragment) response).getActivity();
+        } else {
+            context = (Context) response;
+        }
+
+        final Context finalContext = context;
+        AbHttpUtil.getInstance(context).get(SERVICE_URL + url, params, new AbStringHttpResponseListener() {
+                    @Override
+                    public void onSuccess(int statusCode, String content) {
+
+                        content = AbBase64.decode(content, "UTF-8");
+                        Log.e("接口返回的数据=", content);
                         try {
                             JSONObject jo = new JSONObject(content);
                             response.onMessageResponse(url, jo);
